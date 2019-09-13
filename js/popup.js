5dbcf0377022322ca083c55d0952cf4f4d0bf52a
Daniyar <mr.f1eeman@gmail.com>
2019-09-13 20:55:37 +0600

    FINISH
    js - модальное окно
    Карта гугл

--- js/popup.js
+++ js/popup.js
@@ -1,6 +1,26 @@
-const btnm = document.querySelector(".open-modal-popup");
-const formn = document.querySelector(".appointment-form");
-btnm.addEventListener("click", function(evt) {
+const openModal = document.querySelector(".open-modal-popup");
+const popup = document.querySelector(".modal-popup");
+const form = popup.querySelector("form");
+const arrivalDate = form.querySelector("[name=arrival-date]")
+const departureDate = form.querySelector("[name=departure-date]")
+openModal.addEventListener("click", function(evt) {
     evt.preventDefault();
-    formn.classList.toggle("modal-show");
+    popup.classList.toggle("modal-show");
+    popup.classList.remove("modal-error");
+    arrivalDate.focus();
+});
+form.addEventListener("submit", function(evt) {
+    if (!arrivalDate.value || !departureDate.value) {
+        evt.preventDefault();
+        popup.classList.remove("modal-error");
+        popup.offsetWidth = popup.offsetWidth;
+        popup.classList.add("modal-error");
+    }
+});
+window.addEventListener("keydown", function(evt) {
+    if ((evt.keyCode === 27) && (popup.classList.contains("modal-show"))) {
+        evt.preventDefault();
+        popup.classList.remove("modal-show");
+        popup.classList.remove("modal-error");
+    }
 });
